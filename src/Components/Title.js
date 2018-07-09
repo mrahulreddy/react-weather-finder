7f01e6a16dbd8d670f6980b95be214f0320511ae
mrahulreddy <rmunakala@evoketechnologies.com>
2018-07-09 13:46:45 +0530

    changed title to stateless component

--- src/Components/Title.js
+++ src/Components/Title.js
@@ -1,12 +1,9 @@
-import React, { Component } from 'react'
 
-export default class Titles extends Component {
-  render () {
-    return (
+import React, { Component } from 'react'
+const Titles = () => (
       <div>
         <h1 className="title-container__title"> Weather Finder </h1>
         <h3 className="title-container__subtitle"> Find out temperature, conditions and more... </h3>
       </div>
-    );
-  };
-}
+)
+export default Titles;
