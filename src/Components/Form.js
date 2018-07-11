0c5ce36430ba65794eb948cecbeacabb58c825a3
mrahulreddy <rmunakala@evoketechnologies.com>
2018-07-11 12:30:03 +0530

    changed state to stateless component

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react'
 
-export default class Form extends Component {
-  render () {
-    return (
-          <form onSubmit={this.props.getWeather}>
+const Form = (props) => (
+          <form onSubmit={props.getWeather}>
               <input type="text" name="city" placeholder="City..." />
               <input type="text" name="country" placeholder="Country..." />
               <button> Get Weather </button>
           </form>
-    )
-  };
-}
+    );
+    
+export default Form;
